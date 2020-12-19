dac7cef2d4e20c629e3435778a9049f3aed022c7
Zeliper <dhtmddn13213@naver.com>
2020-12-20 00:06:07 +0900

    Code Refactoring
    issues : #1

--- 1stStepC++/첫번째.cpp
+++ 1stStepC++/첫번째.cpp
@@ -1,38 +1,55 @@
 #include <iostream>
 using namespace std;
 
-
 namespace mycode {
 	void foo() {
 		cout << 4 * 9 << endl;;
 	}
-
-
 }
-int main() {
-	int x;
-	//int uninitializedvalue=rand();
-	int initializedvalue = 19;
-	float somenum = 0;
-	int ab = 24;
-	std::cout << "Hello, World!" << std::endl;
-	printf("Hello, World!\n");
-	std::cout << "오늘부터 1일" << std::endl;
-	cout << "아무거나 입력" << endl;
-	std::cin >> x;
-	std::cout << "그거슨 "<<x<<"였구요\n"<< std::endl;
-	mycode::foo();
-	//cout << uninitializedvalue <<" is random"<< endl;
-	cout << initializedvalue << endl;
-	cout << "아무거나 입력2" << endl;
-	cin >> somenum;
 
+int main() {
+	cout << "오늘부터 1일" << endl <<endl;
+
+	//CPP std::cout 함수와 C printf함수 비교
+	std::cout << "Hello, World!" << endl;
+	printf("Hello, World!\n");
+
+	//int 형식 변수 cin으로 입력 받기
+	int x;
+	cout << "아무 정수(int) 입력 : ";
+	std::cin >> x;
+	std::cout << "그거슨 " << x << "였구요\n" << std::endl;
+	
+	//foo()함수 실행
+	cout << "foo()함수 실행 결과 :";
+	mycode::foo();
+
+	//사용하지 않는 코드 #초기화 상태에 따른 결과 비교 코드로 보임.
+	/*int uninitializedvalue=rand();
+	int initializedvalue = 19;
+	cout << uninitializedvalue <<" is random"<< endl;
+	cout << initializedvalue << endl;*/
+
+	//float 형식 변수 cin으로 입력 받기
+	float somenum = 0;
+	cout << "아무 실수(float) 입력 : ";
+	cin >> somenum;
+	
+	//형식 변환 방법에 따른 출력 비교
+	int i = (int)somenum;
+	int i2 = int(somenum);
+	int i3 = static_cast<int>(somenum);
+	cout << "(int)somenum : " << i << endl;
+	cout << "int(somenum) : " << i2 << endl;
+	cout << "static_cast<int>(somenum) : " << i3 << endl;
+
+	//아래로는 원래 코드
 	/*int i = (int)somenum;
 	int i2 = int(somenum);
 	int i3 = static_cast<int>(somenum);
 	cout << somenum << "\n" << i << "\n" << i2 << "\n" << i3 << endl;
 	*/
-	if (somenum > 3) {
+	/*if (somenum > 3) {
 		int i1 = (int)somenum;
 		cout << i1 << endl;
 	}
@@ -45,6 +62,6 @@ int main() {
 		int i3 = static_cast<int>(somenum);
 
 		cout << i3 << endl;
-	}
+	}*/
 	return 0;
 }
